9791a0c2f6ef1426087734237784ac6e7e3719f1
seanxiao <13700324600@163.com>
2019-07-29 17:08:26 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just test',
+  themeConfig: {
+    sidebar: [
+      {
+        title: '数据库',
+        children: [
+          {
+            title: 'mysql',
+            children: [
+              '/database/mysql/1',
+              '/database/mysql/2'
+            ]
+          },
+          {
+            title: 'redis',
+            children: [
+              '/database/redis/1',
+              '/database/redis/2'
+            ]
+          },
+          {
+            title: 'mongoDB',
+            children: [
+              '/database/mongoDB/1',
+              '/database/mongoDB/2'
+            ]
+          }
+        ]
+      },
+      {
+        title: '数据结构与算法',
+        children: [
+          '/algorithms/1',
+          '/algorithms/2'
+        ]
+      }
+    ]
+  },
+}
