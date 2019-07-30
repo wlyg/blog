ac02b720f3674a6034f5494afe807016f0d0c0ab
sean <13700324600@163.com>
2019-07-30 20:48:10 +0800

    blog

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,53 +1,55 @@
-function integrateGitalk(router) {
-  const linkGitalk = document.createElement('link');
-  linkGitalk.href = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css';
-  linkGitalk.rel = 'stylesheet';
-  document.body.appendChild(linkGitalk);
-  const scriptGitalk = document.createElement('script');
-  scriptGitalk.src = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js';
-  document.body.appendChild(scriptGitalk);
+function integrateGitment(router) {
+  const linkGitment = document.createElement('link')
+  linkGitment.href = 'https://imsun.github.io/gitment/style/default.css'
+  linkGitment.rel = 'stylesheet'
+  const scriptGitment = document.createElement('script')
+  document.body.appendChild(linkGitment)
+  scriptGitment.src = 'https://imsun.github.io/gitment/dist/gitment.browser.js'
+  document.body.appendChild(scriptGitment)
+
   router.afterEach((to) => {
-    if (scriptGitalk.onload) {
-      loadGitalk(to);
+    if (scriptGitment.onload) {
+      renderGitment()
     } else {
-      scriptGitalk.onload = () => {
-        loadGitalk(to);
+      scriptGitment.onload = () => {
+        const commentsContainer = document.createElement('div')
+        commentsContainer.id = 'comments-container'
+        commentsContainer.classList.add('content')
+        const $page = document.querySelector('.page')
+        if ($page) {
+          $page.appendChild(commentsContainer)
+          renderGitment()
+        }
       }
     }
-  });
-  function loadGitalk(to) {
-    let commentsContainer = document.getElementById('gitalk-container');
-    if (!commentsContainer) {
-      commentsContainer = document.createElement('div');
-      commentsContainer.id = 'gitalk-container';
-      commentsContainer.classList.add('content');
-    }
-    const $page = document.querySelector('.page');
-    if ($page) {
-      $page.appendChild(commentsContainer);
-      if (typeof Gitalk !== 'undefined' && Gitalk instanceof Function) {
-        renderGitalk(to.fullPath);
-      }
-    }
-  }
-  function renderGitalk(fullPath) {
-    const gitalk = new Gitalk({
-      clientID: 'e62dbe7763ccbf467cda',
-      clientSecret: '9b20eb399568a4a71e89224106c19725b2ef856c',
-      repo: 'comment',
-      owner: 'wlyg',
-      admin: ['wlyg'],
-      id: 'comment',
-      distractionFreeMode: false,
-      language: 'zh-CN',
-    });
-    gitalk.render('gitalk-container');
+  })
+
+  function renderGitment() {
+    const gitment = new Gitment({
+      // ！！！ID最好不要使用默认值（location.href），因为href会携带hash，可能导致一个页面对应像个评论issue！！！
+      // https://github.com/imsun/gitment/issues/55
+      id: location.pathname,
+      owner: 'wlyg', // 必须是你自己的github账号
+      repo: 'blog', // 上一个准备的github仓库
+      link: location.origin + location.pathname,
+      oauth: {
+        client_id: 'e62dbe7763ccbf467cda', // 第一步注册 OAuth application 后获取到的 Client ID
+        client_secret: '9b20eb399568a4a71e89224106c19725b2ef856c', // 第一步注册 OAuth application 后获取到的 Clien Secret
+      },
+    })
+    gitment.render('comments-container')
   }
 }
-  
-export default ({ Vue, options, router }) => {
+
+export default ({
+  Vue, // VuePress 正在使用的 Vue 构造函数
+  options, // 附加到根实例的一些选项
+  router, // 当前应用的路由实例
+  siteData // 站点元数据
+}) => {
   try {
-    document && integrateGitalk(router)
+    // 生成静态页时在node中执行，没有document对象
+    document && integrateGitment(router)
   } catch (e) {
     console.error(e.message)
   }
